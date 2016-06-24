97f6f848baeae40d8846700ecfbb2776acb10526
isaolmez <isaolmez@gmail.com>
2016-06-24 09:30:52 +0300

    update

---
+++ src/main/java/com/isa/patterns/simplefactory/ClientMain.java
@@ -0,0 +1,10 @@
+package com.isa.patterns.simplefactory;
+
+public class ClientMain {
+public static void main(String[] args) {
+	PizzaStore store = new PizzaStore(new SimplePizzaFactory());
+	store.orderPizza("cheese");
+	System.out.println("*****************************");
+	store.orderPizza("clam");
+}
+}
