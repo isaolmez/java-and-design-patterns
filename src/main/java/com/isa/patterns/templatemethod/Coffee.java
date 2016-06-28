4c6d2163e0f3b47428568ada5080eaacb7580ea9
isaolmez <isaolmez@gmail.com>
2016-06-28 11:06:46 +0300

    template method pattern added

---
+++ src/main/java/com/isa/patterns/templatemethod/Coffee.java
@@ -0,0 +1,15 @@
+package com.isa.patterns.templatemethod;
+
+public class Coffee extends CaffeineBeverage{
+
+	@Override
+	public void brew() {
+		System.out.println("Dripping coffee");
+	}
+
+	@Override
+	public void addContiments() {
+		System.out.println("Adding sugar and milk");
+	}
+
+}
