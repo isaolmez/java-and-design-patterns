8b284180dbdf94f8e7c936421ece995a34ffffda
isaolmez <isaolmez@gmail.com>
2016-09-26 00:04:23 +0300

    update

--- src/main/java/com/isa/patterns/state/State.java
+++ src/main/java/com/isa/patterns/state/State.java
@@ -1,5 +1,8 @@
 package com.isa.patterns.state;
-
+/**
+ * State pattern resembles to strategy patten in that both promotes algorithms/strategies changable at runtime. But their purpose is different. 
+ * Strategy clients use one strategy and perform with it. State clients are bound to use different state objects changing constantly at runtime. 
+ */
 public interface State {
 	void insertQuarter();
 
