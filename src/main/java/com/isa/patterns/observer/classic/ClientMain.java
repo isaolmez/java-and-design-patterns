f542898234fdda36284694c3fcba967e1023de9a
isaolmez <isaolmez@gmail.com>
2020-05-31 00:39:51 +0300

    Update observer pattern example

--- src/main/java/com/isa/patterns/observer/classic/ClientMain.java
+++ src/main/java/com/isa/patterns/observer/classic/ClientMain.java
@@ -5,19 +5,16 @@ import java.util.Random;
 public class ClientMain {
 
     public static void main(String[] args) {
-        WeatherStation weatherStation = new WeatherStation();
+        final WeatherStation weatherStation = new WeatherStation();
 
-        CurrentConditions currentConditions = new CurrentConditions();
-        ForecastDisplay forecastDisplay = new ForecastDisplay();
-        StatisticsDisplay statisticsDisplay = new StatisticsDisplay();
-        StatisticsDisplay secondStaticsDisplay = new StatisticsDisplay();
-
+        final CurrentConditions currentConditions = new CurrentConditions();
         weatherStation.registerObserver(currentConditions);
+        final ForecastDisplay forecastDisplay = new ForecastDisplay();
         weatherStation.registerObserver(forecastDisplay);
+        final StatisticsDisplay statisticsDisplay = new StatisticsDisplay();
         weatherStation.registerObserver(statisticsDisplay);
-        weatherStation.removeObserver(secondStaticsDisplay);
 
-        Random random = new Random();
+        final Random random = new Random();
         for (int i = 0; i < 5; i++) {
             weatherStation.setMeasurements(random.nextFloat(), random.nextFloat(), random.nextFloat());
             System.out.println("***************************************");
